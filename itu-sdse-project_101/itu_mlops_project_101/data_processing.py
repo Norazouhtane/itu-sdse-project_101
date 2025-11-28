4239a5bae06163288ce45ff18ea732c6054aaeda
Karima-m <karima.02@outlook.dk>
2025-11-28 16:29:23 +0100

    feat(data_processing): Read data and filter by dates

--- itu-sdse-project_101/itu_mlops_project_101/data_processing.py
+++ itu-sdse-project_101/itu_mlops_project_101/data_processing.py
@@ -17,3 +17,17 @@ def impute_missing_values(x, method="mean"):
     else:
         x = x.fillna(x.mode()[0])
     return x
+
+
+# Read and filter data based on dates
+data = pd.read_csv("raw_data.csv")
+
+max_date = "2024-01-31"
+min_date = "2024-01-01"
+
+max_date = pd.to_datetime(max_date).date()
+min_date = pd.to_datetime(min_date).date()
+
+data["date_part"] = pd.to_datetime(data["date_part"]).dt.date
+data = data[(data["date_part"] >= min_date) & (data["date_part"] <= max_date)]
+
