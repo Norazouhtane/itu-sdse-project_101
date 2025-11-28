9e781f4a34dcb470a8d6c395b63138e2cfb5b863
Karima-m <karima.02@outlook.dk>
2025-11-28 17:39:28 +0100

    feat(data_processing): feature selection and data cleaning

--- itu-sdse-project_101/itu_mlops_project_101/data_processing.py
+++ itu-sdse-project_101/itu_mlops_project_101/data_processing.py
@@ -31,3 +31,35 @@ min_date = pd.to_datetime(min_date).date()
 data["date_part"] = pd.to_datetime(data["date_part"]).dt.date
 data = data[(data["date_part"] >= min_date) & (data["date_part"] <= max_date)]
 
+
+# Feature selection
+data = data.drop(
+    [
+        "is_active", 
+        "marketing_consent", 
+        "first_booking", 
+        "existing_customer", 
+        "last_seen"
+    ],
+    axis=1
+)
+
+data = data.drop(
+    [
+        "domain", 
+        "country", 
+        "visited_learn_more_before_booking", 
+        "visited_faq"
+    ],
+    axis=1
+)
+
+
+# Data cleaning
+data["lead_indicator"].replace("", np.nan, inplace=True)
+data["lead_id"].replace("", np.nan, inplace=True)
+
+data = data.dropna(axis=0, subset=["lead_indicator"])
+data = data.dropna(axis=0, subset=["lead_id"])
+
+data = data[data.source == "signup"]
