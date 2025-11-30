8a65009e3219b038ab93775f3307f9aa44975bae
feliciavs24 <feso@itu.dk>
2025-11-30 14:37:20 +0100

    feat(model_selection): update with another helper function and transition model into staging

--- itu-sdse-project_101/itu_mlops_project_101/model_selection.py
+++ itu-sdse-project_101/itu_mlops_project_101/model_selection.py
@@ -8,7 +8,9 @@ import pandas as pd
 from mlflow.tracking.client import MlflowClient
 from mlflow.entities.model_registry.model_version_status import ModelVersionStatus
 
+client = MlflowClient()
 
+# Helper functions
 def wait_until_ready(model_name, model_version):
     client = MlflowClient()
     for _ in range(10):
@@ -21,6 +23,20 @@ def wait_until_ready(model_name, model_version):
         if status == ModelVersionStatus.READY:
             break
         time.sleep(1)
+
+def wait_for_deployment(model_name, model_version, stage='Staging'):
+    status = False
+    while not status:
+        model_version_details = dict(
+            client.get_model_version(name=model_name,version=model_version)
+            )
+        if model_version_details['current_stage'] == stage:
+            print(f'Transition completed to {stage}')
+            status = True
+            break
+        else:
+            time.sleep(2)
+    return status
 
 
 # Define constants
@@ -62,3 +78,19 @@ if run_id is not None:
     model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
     wait_until_ready(model_details.name, model_details.version)
     model_details = dict(model_details)
+
+
+#Transition to staging
+model_version = 1
+
+model_version_details = dict(client.get_model_version(name=model_name,version=model_version))
+model_status = True
+if model_version_details['current_stage'] != 'Staging':
+    client.transition_model_version_stage(
+        name=model_name,
+        version=model_version,stage="Staging", 
+        archive_existing_versions=True
+    )
+    model_status = wait_for_deployment(model_name, model_version, 'Staging')
+else:
+    print('Model already in staging')
