ddbdd198dccf73e15eb4f9d2575d4bc87c688f80
Norazouhtane <nozo@itu.dk>
2025-12-09 10:04:33 +0100

    chore: change model name

--- itu-sdse-project_101/itu_mlops_project_101/model_training.py
+++ itu-sdse-project_101/itu_mlops_project_101/model_training.py
@@ -116,7 +116,7 @@ experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
 with mlflow.start_run(experiment_id=experiment_id) as run:
     model = LogisticRegression()
-    lr_model_path = "/project/models/lead_model_lr.pkl"
+    lr_model_path = "/project/models/model.pkl"
 
     params = {
               'solver': ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
